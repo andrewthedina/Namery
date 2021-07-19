58e9de5c266ea76ebc2e15be49d079346baa30fd
andrewthedina <andrewthedina@icloud.com>
2021-07-18 20:08:58 -0400

    adds NameryController and Namery/Index.cshtml view

---
+++ Namery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Namery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
