18c3802213e3d32fb398b0877aa09e21c784eb00
andrewthedina <andrewthedina@icloud.com>
2021-07-18 20:56:21 -0400

    adds seed data model (not yet triggered automatically) adds db connection

---
+++ Namery/Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Namery.Models;
+//using MvcPerson.Data;
+using System;
+using System.Linq;
+
+namespace MvcPerson.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcPersonContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcPersonContext>>()))
+            {
+                // Look for any people.
+                if (context.Person.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Person.AddRange(
+                    new Person
+                    {
+                        Id = 1,
+                        Name = "Andrew Dina",
+                        Notes = "I am you!"
+                    },
+
+                    new Person
+                    {
+                        Id = 1,
+                        Name = "Lisa Dina",
+                        Notes = "Wife"
+                    },
+
+                    new Person
+                    {
+                        Id = 1,
+                        Name = "Andrea Dina",
+                        Notes = "Daughter"
+                    },
+
+                    new Person
+                    {
+                        Id = 1,
+                        Name = "Timothy Dina",
+                        Notes = "Son"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
